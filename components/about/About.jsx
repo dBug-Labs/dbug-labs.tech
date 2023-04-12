e7ea2787d24b52d9eb857333e726483b38ed3804
tanmayagrwl <tanmayhp78@gmail.com>
2023-04-13 01:21:25 +0530

    tan/complete about page -NRM

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -1,24 +1,37 @@
 import aboutsvg from "./aboutsvg.svg" ;
+import vector1 from "./vector1.svg" ;
+import vector2 from "./vector2.svg" ;
+import vector3 from "./vector3.svg" ;
+import vector4 from "./vector4.svg" ;
 
 function About() {
   return (
-    <div className="h-screen max-h-screen w-full max-w-screen-2xl bg-black pl-16 pt-16 ">
+    <div className=" w-full  bg-black pl-16 pt-16 ">
       
 
-      <div className="flex justify-between pr-6">
-        <div className="text-white w-[50%]">
-          <h1 className="text-white text-6xl pb-14">ABOUT DBUG LABS</h1>
-          <h1 className=" text-xl">
-            So introducing dBug Labs, a student-run club dedicated to the art of debugging & testing! We are a group of passionate students interested in solving complex problems through effective debugging techniques. Our mission is to provide a supportive community for individuals who want to enhance their debugging skills and share their knowledge with others. We strive to create a fun and collaborative environment where students can collaborate to share ideas, work on projects, and learn from each other. 
+      <div className="flex flex-col sm:flex-col md:flex-row justify-between">
+        <div className="text-white w-[80%] smw-[80%] lg:w-[50%]">
+          <h1 className="text-white text-6xl pb-20">ABOUT DBUG LABS</h1>
+          <h1 className="text-lg sm:text-lg lg:text-xl ">
+         dBug Labs, a student-run club dedicated to the art of debugging & testing! We are a group of passionate students interested in solving complex problems through effective debugging techniques. Our mission is to provide a supportive community for individuals who want to enhance their debugging skills and share their knowledge with others. We strive to create a fun and collaborative environment where students can collaborate to share ideas, work on projects, and learn from each other. 
           </h1>
         </div>        
 
         <div>
-          <img className="" src={aboutsvg} alt="image" />
+          <div className="flex w-full justify-between">
+            <img className="relative"  src={vector3} alt="image" />
+            <img className="alig"  src={vector1} alt="image" />
+          </div>
+          <div>
+            <img className="pr-0 sm:pr-0 lg:pr-16   " src={aboutsvg} alt="image" />
+            </div>
         </div>  
-
-
       </div>
+      <div className="flex w-full justify-between ">
+            <img  src={vector3} alt="image" />
+            <img  src={vector4} alt="image" />
+            <img  src={vector2} alt="image" />
+          </div>
     </div>
   );
 }
