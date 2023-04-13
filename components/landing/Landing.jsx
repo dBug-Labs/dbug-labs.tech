ed7820755114841d2e7f3b811043babc182e4cfa
Ash <01tripster@gmail.com>
2023-04-14 05:12:30 +0530

    feat: add landing page animation

--- components/landing/Landing.jsx
+++ components/landing/Landing.jsx
@@ -1,8 +1,31 @@
 import { Loader } from "../loader/Loader";
+import { useLayoutEffect } from "react";
 
 function Landing() {
+  useLayoutEffect(()=>{
+    console.log("uselayout effect entered")
+    const loader = document.querySelector(".loader");
+    const headingPara = document.querySelector(".headingPara");
+    const headingdesc = document.querySelector(".headingdesc");
+    loader.addEventListener("animationend", ()=>{
+      console.log("animation end")
+      loader.classList.remove("bg-black");
+      headingPara.classList.remove("hidden")
+      headingdesc.classList.remove("hidden")
+      document.querySelector(".topleftcirlce").remove();
+
+    })
+  })
+
   return (
-    <div className="h-screen overflow-hidden">
+    <div className="h-screen overflow-hidden bg-black">
+      <div className="headingPara hidden text-4xl text-white relative top-56 left-20">
+        <div>"Every bug is just a hidden feature</div>
+        <div> &nbsp; waiting to be discovered!"</div>
+      </div>
+
+      <div className="headingdesc hidden text-white text-xl max-w-[60%] relative top-64 left-20">In the programming and software development world, debugging is essential, and having a community to learn from and practice can be very beneficial. Also, companies like JP Morgan and Wells Fargo do unit testing before the software development, this shows the importance of unit testing and debugging in today's world.</div>
+
       <Loader/>
     </div>
   );
--- components/loader/Loader.jsx
+++ components/loader/Loader.jsx
@@ -1,12 +1,13 @@
+import { useLayoutEffect } from "react";
 import landingCircle from "./landingCircle.svg"
 
 function Loader() {
   return (
-    <div className="h-screen overflow-hidden flex justify-center items-center bg-black">
+    <div className="loader h-screen overflow-hidden flex justify-center items-center bg-black ">
       <img src={landingCircle} alt="topleftimage" className="topleftcirlce"/>
       <img src={landingCircle} alt="topleftimage" className="toprightcircle"/>
       <img src={landingCircle} alt="topleftimage" className="bottomrightcircle"/>
-      <h1 className="loaderHeading font-semibold text-5xl text-white">DBUG LABS</h1>
+      <h1 className="loaderHeading font-semibold text-5xl text-white absolute">DBUG LABS</h1>
     </div>
   );
 }
